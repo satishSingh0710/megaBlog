b8d712958e04afda42f8aa0cbd4b36bef9d1dcaa
LostLord07 <satishsingh45264209@gmail.com>
2024-02-19 01:28:14 +0530

    login form

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,14 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
 export {
+    Input,
     Header,
     Container,
     Footer, 
     Logo, 
-    LogoutBtn
+    LogoutBtn, 
+    Button
 }; 
